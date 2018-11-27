742d054246438df834aa82f83803dff174747eeb
msyed3 <msyed3@192.168.0.14>
2018-11-27 00:16:59 -0500

    Missed one file

---
+++ chwilio/src/main/java/com/chwilio/model/Tweet.java
@@ -0,0 +1,24 @@
+package com.chwilio.model;
+
+public class Tweet {
+	private String id;
+    private String text;
+    
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public Tweet(String id, String text) {
+		this.id = id;
+		this.text = text;
+	}
+}
